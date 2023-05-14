1219fb5f1d8284bbf4abeea4020de97a87784bff
shirohebiui <shirohebiui@gmail.com>
2023-05-15 08:55:35 +0900

    git rm dir

--- linux/5w/stat.c
+++
@@ -1,9 +0,0 @@
-  #include <sys/types.h>
-#include <sys/stat.h>
-
-int main()
-{
-	int stat(const char *filename, struct stat *buf);
-	int fstat(int fd, struct stat *buf);
-	int lstat(const char *filename, struct stat *buf);
-}
